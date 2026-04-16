c04fd37476ac600a2afe0f2f6c69683003ede5a6
xixinsama <95458273+xixinsama@users.noreply.github.com>
2026-04-16 23:45:00 +0800

    初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # rouge_knight
+
 肉鸽骑士（暂名）
+
+该项目基于MIT协议永久开源，以供学习
+
+[项目策划案](https://docs.qq.com/sheet/DZXdHUUxoVVBMZnRC)
+
+该文档中包含所有素材的引用
---
+++ player/player.gd
@@ -0,0 +1,11 @@
+extends Node2D
+class_name Player
+
+static var current: Player
+
+const MoveUp: StringName = &"move_up"
+const MoveDown: StringName = &"move_down"
+const MoveLeft: StringName = &"move_left"
+const MoveRight: StringName = &"move_right"
+const Dash: StringName = &"dash"
+const Run: StringName = &"run"
