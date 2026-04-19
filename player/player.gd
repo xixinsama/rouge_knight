fa3c2e72487a24e9e1d79c7925a54c335cbbc387
Fxy115 <180768868@qq.com>
2026-04-19 21:48:15 +0800

    完成了状态机框架

--- player/player.gd
+++ player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 class_name Player
 
-static var current: Player
+static var instances: Dictionary
 
 const MoveUp: StringName = &"move_up"
 const MoveDown: StringName = &"move_down"
@@ -9,9 +9,28 @@ const MoveLeft: StringName = &"move_left"
 const MoveRight: StringName = &"move_right"
 const Dash: StringName = &"dash"
 const Run: StringName = &"run"
-<<<<<<< HEAD
-=======
+
+
+@export_group("状态")
+##状态	[br]
+var status :Dictionary:
+	set(value):
+		move=value.move
+		proactive=value.proactive
+		passive=value.passive
+
+
+##移动状态机
+var move :StatusManage.MoveBase
+
+##主动状态机
+var proactive :StatusManage.ProactiveBase
+
+##被动状态机
+var passive :StatusManage.PassiveBase
+
+@export_group("")
 
 func _ready() -> void:
+	self.status=StatusManage.get_status()
 	pass
->>>>>>> main
--- scripts/base/status_manage.gd
+++ scripts/base/status_manage.gd
@@ -1,12 +1,12 @@
-##状态机基类
-##单例调用(挂载到全局自动加载)
-##建造者模式
+##状态机基类	[br]
+##单例调用(挂载到全局自动加载)	[br]
+##建造者模式	[br]
 extends Node
 class_name StatusManage
 
 @export_group("状态枚举")
-##状态枚举
-enum MOVE_BASE{
+##移动状态枚举
+enum MOVE_LIST{
 	##默认基类
 	MOVE_BASE,
 	##待机
@@ -22,32 +22,75 @@ enum MOVE_BASE{
 	MAX,
 }
 
+##主动状态枚举
+enum PROACTIVE_LIST{
+	##默认基类
+	PROACTIVE_BASE,
+	##攻击状态
+	ATTACK,
+	##闪避状态
+	DODGE,
+	MAX,
+}
 
-##产品仓库
-var warehouse :Dictionary[String,Variant] = {}
-##产品原型
-var status :Dictionary[String,Status]
+##被动状态枚举
+enum PASSIVE_LIST{
+	##默认基类
+	PASSIVE_BASE,
+	##僵直状态
+	STUN,
+	MAX
+}
+@export_group("")
+
+
+##产品仓库(状态列表)
+static var _warehouse :Dictionary[int,Dictionary] = {}
 
 func _ready() -> void:
-#	移动状态
-	self.status.MoveBase=Idle.new()
-#	主动状态
-	self.status.ProactiveBase=ProactiveDefault.new()
-#	被动状态
-	self.status.PassiveBase=PassiveDefault.new()
 	pass
 
 
-##定制
-func custom():
-	
-	pass
+##获取默认标识
+static func get_base_key() -> Dictionary[String,int]:
+	return {
+		##移动状态
+		move=MOVE_LIST.MOVE_BASE,
+		##主动状态
+		proactive=PROACTIVE_LIST.PROACTIVE_BASE,
+		##被动状态
+		passive=PASSIVE_LIST.PASSIVE_BASE,
+	}
+
+##获取默认状态机
+static func get_base_status() -> Dictionary[String,Variant]:
+	return {
+		##移动状态
+		move=MoveBase.new(),
+		##主动状态
+		proactive=ProactiveBase.new(),
+		##被动状态
+		passive=PassiveBase.new(),
+	}
+
+##获取状态机
+static func get_status(key :Dictionary[String,int]={}) -> Dictionary[String,Variant]:
+	if key.is_empty():
+		key=StatusManage.get_base_key()
+	var hash_key=hash(key)
+	if StatusManage._warehouse.get(hash_key) == null:
+		##组装并入库
+		StatusManage._warehouse[hash_key]=StatusManage._assemble_status(key)
+	return StatusManage._warehouse[hash_key]
 
 
-
-
-
-
+##组装状态机
+static func _assemble_status(key :Dictionary[String,int]) -> Dictionary[String,Variant]:
+	var base_status = StatusManage.get_base_status();
+	base_status.move.enter(base_status,key.move)
+	base_status.proactive.enter(base_status,key.proactive)
+	base_status.passive.enter(base_status,key.passive)
+	return base_status
 
 
 ##状态基类
@@ -64,90 +107,82 @@ class Status:
 ##chase 追击
 ##dash	冲刺
 class MoveBase extends Status:
-	func enter(status,enter :StatusManage.MOVE_BASE):
-		if enter == MOVE_BASE.MOVE_BASE :
-			status.move_base=MoveBase.new()
-		elif enter == MOVE_BASE.IDLE :
-			status.move_base=Idle.new()
-		elif enter == MOVE_BASE.WALK :
-			status.move_base=Walk.new()
-		elif enter == MOVE_BASE.RUN :
-			status.move_base=Idle.new()
+	func enter(status,enter :StatusManage.MOVE_LIST):
+		if enter == StatusManage.MOVE_LIST.MOVE_BASE :
+			status.move=MoveBase.new()
+		elif enter == StatusManage.MOVE_LIST.IDLE :
+			status.move=Idle.new()
+		elif enter == StatusManage.MOVE_LIST.WALK :
+			status.move=Walk.new()
+		elif enter == StatusManage.MOVE_LIST.RUN :
+			status.move=Run.new()
+		elif enter == StatusManage.MOVE_LIST.CHASE :
+			status.move=Chase.new()
+		elif enter == StatusManage.MOVE_LIST.DASH :
+			status.move=Dash.new()
+		else :
+			status.move=MoveBase.new()
 		pass
 
+
 ##主动状态基类
-@abstract
 class ProactiveBase extends Status:
-	@abstract
-	func enter(status,enter :ProactiveBase)
+	##状态切换
+	func enter(status,enter :StatusManage.PROACTIVE_LIST):
+		if enter==StatusManage.PROACTIVE_LIST.PROACTIVE_BASE:
+			status.proactive=ProactiveBase.new()
+		elif enter==StatusManage.PROACTIVE_LIST.ATTACK:
+			status.proactive=Attack.new()
+		elif enter==StatusManage.PROACTIVE_LIST.DODGE:
+			status.proactive=Dodge.new()
+		else :
+			status.proactive=ProactiveBase.new()
+
 
 ##被动状态基类
-@abstract
 class PassiveBase extends Status:
-	@abstract
-	func enter(status,enter :PassiveBase)
+	func enter(status,enter :StatusManage.PASSIVE_LIST):
+		if enter==StatusManage.PASSIVE_LIST.PASSIVE_BASE:
+			status.passive=PassiveBase.new()
+		elif enter==StatusManage.PASSIVE_LIST.STUN:
+			status.passive=Stun.new()
+		else :
+			status.passive=PassiveBase.new()
+		pass
 
 
 
 
 ##待机状态
 class Idle extends MoveBase:
-	func enter(status,enter :StatusManage.MOVE_BASE):
-		status.MoveBase=enter
-		return status.MoveBase
+	pass
 
 ##移动状态
 class Walk extends MoveBase:
-	func enter(status,enter :StatusManage.MOVE_BASE):
-		status.MoveBase=enter
-		return status.MoveBase
+	pass
 
 ##快速移动状态
 class Run extends MoveBase:
-	func enter(status,enter :StatusManage.MOVE_BASE):
-		status.MoveBase=enter
-		return status.MoveBase
+	pass
 
 ##追击状态
 class Chase extends MoveBase:
-	func enter(status,enter :StatusManage.MOVE_BASE):
-		status.MoveBase=enter
-		return status.MoveBase
+	pass
 
 ##冲刺状态
 class Dash extends MoveBase:
-	func enter(status,enter :StatusManage.MOVE_BASE):
-		status.MoveBase=enter
-		return status.MoveBase
+	pass
 
-
-##主动默认状态
-class ProactiveDefault extends ProactiveBase:
-	func enter(status,enter :ProactiveBase):
-		status.ProactiveBase=enter
-		return status.ProactiveBase
 
 ##攻击状态
 class Attack extends ProactiveBase:
-	func enter(status,enter :ProactiveBase):
-		status.ProactiveBase=enter
-		return status.ProactiveBase
-
-##被动默认状态
-class PassiveDefault extends PassiveBase:
-	func enter(status,enter :PassiveBase):
-		status.PassiveBase=enter
-		return status.PassiveBase
+	pass
 
 ##闪避状态
-class Dodge extends PassiveBase:
-	func enter(status,enter :PassiveBase):
-		status.PassiveBase=enter
-		return status.PassiveBase
+class Dodge extends ProactiveBase:
+	pass
 
 
 ##僵直状态
 class Stun extends PassiveBase:
-	func enter(status,enter :PassiveBase):
-		status.PassiveBase=enter
-		return status.PassiveBase
+	pass
